16112a65408a36b2b1b6052cbe13c8413d6888ec
gustavomaes <gustavomaescd@gmail.com>
2019-03-05 06:00:27 -0300

    add theme file

---
+++ src/theme.ts
@@ -0,0 +1,15 @@
+const theme = {
+    colors: {
+        primary: "#307ba5",
+        primaryLight: "#81b9d4",
+        secondary: "#1abc9c",
+        primaryBackground: "#edf3f8",
+        secondaryBackground: "#fff",
+        shadow: "0 2px 1px rgba(0,0,0,.1)",
+        border: "#ccc",
+        textPrimary: "#000",
+        textLight: "#fff",
+    }
+}
+
+export default theme
